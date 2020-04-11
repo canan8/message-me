918be961125d9d1e351183b9fdbe115d2394a92b
Canan <canan.ercan@parasut.com>
2020-04-12 00:53:16 +0300

    Show messages from db in chatroom

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: "Hey it's Rick", user_id: "2")
+Message.create(body: "Hey it's Morty", user_id: "3")
