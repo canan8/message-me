28e32e0896b77566448aa7cfadb291a168af286a
Canan <cananercan96@gmail.com>
2020-04-26 11:44:01 +0300

    Add flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,7 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
