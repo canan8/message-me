1b304ed4e6daebdb37bcd331d844a96b5b27352d
Canan <cananercan96@gmail.com>
2020-04-29 00:16:09 +0300

    Broadcast messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
 
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel",
+                                   modified_message: message_render(message)
     end
   end
 
@@ -13,5 +14,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
+    # message partial'inda kullanilan message'a buradaki parameter'i pass eder
   end
 end
